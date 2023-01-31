3d8fd07e68a53707fc8494701f6b209503ce2295
TonyJem <a.jemanov@gmail.com>
2023-01-31 22:52:03 +0200

    Create draft implementation of Header.View

--- myPlanner/Main/Main.ViewController/SubViews/Header/Header.View.swift
+++ myPlanner/Main/Main.ViewController/SubViews/Header/Header.View.swift
@@ -4,9 +4,45 @@ extension Header {
     
     class View: UIView {
         
+        enum Constants {
+            
+            static let dateViewWidth: CGFloat = 120.0
+            static let dateViewHeight: CGFloat = 30.0
+            
+            static let tabBarViewWidth: CGFloat = 400.0
+            static let tabBarViewHeight: CGFloat = 30.0
+            
+        }
+        
         // MARK: - SubViews
         
-        private lazy var subView = UIView().autolayout()
+        private lazy var dateView: UIView = {
+            let view = UIView().autolayout()
+            view.backgroundColor = .orange
+            return view
+        }()
+        
+        private lazy var tabBarView: UIView = {
+            let view = UIView().autolayout()
+            view.backgroundColor = .blue
+            return view
+        }()
+        
+        private lazy var searchButton: UIButton = {
+            let button = UIButton().autolayout()
+            button.setImage(UIImage(systemName: "magnifyingglass"), for: .normal)
+            button.setTitleColor(.white, for: .normal)
+            button.addTarget(self, action: #selector(didTapOnSearchButton), for: .touchUpInside)
+            return button
+        }()
+        
+        private lazy var settingsButton: UIButton = {
+            let button = UIButton().autolayout()
+            button.setImage(UIImage(systemName: "gearshape"), for: .normal)
+            button.setTitleColor(.white, for: .normal)
+            button.addTarget(self, action: #selector(didTapOnSettingsButton), for: .touchUpInside)
+            return button
+        }()
         
         // MARK: - Init
         
@@ -21,23 +57,47 @@ extension Header {
             fatalError("init(coder:) has not been implemented")
         }
         
+        // MARK: - Actions
+        @objc private func didTapOnSearchButton() {
+            print("🟢 didTapOnSearchButton in Header.View")
+        }
+        
+        @objc private func didTapOnSettingsButton() {
+            print("🟢 didTapOnSearchButton in Header.View")
+        }
+                            
+        
         // MARK: - Private Methods
         
         private func setupView() {
-            backgroundColor = .blue
+            backgroundColor = .systemGray
         }
         
         private func addSubViews() {
-            addSubview(subView)
+            addSubview(dateView)
+            addSubview(tabBarView)
+            addSubview(searchButton)
+            addSubview(settingsButton)
         }
         
         private func setupLayout() {
             NSLayoutConstraint.activate([
                 
-                subView.leadingAnchor.constraint(equalTo: leadingAnchor),
-                subView.topAnchor.constraint(equalTo: topAnchor),
-                subView.trailingAnchor.constraint(equalTo: trailingAnchor),
-                subView.bottomAnchor.constraint(equalTo: bottomAnchor)
+                dateView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: .spacingL),
+                dateView.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -.spacingXXS),
+                dateView.widthAnchor.constraint(equalToConstant: Constants.dateViewWidth),
+                dateView.heightAnchor.constraint(equalToConstant: Constants.dateViewHeight),
+                
+                tabBarView.centerXAnchor.constraint(equalTo: centerXAnchor),
+                tabBarView.bottomAnchor.constraint(equalTo: bottomAnchor),
+                tabBarView.widthAnchor.constraint(equalToConstant: Constants.tabBarViewWidth),
+                tabBarView.heightAnchor.constraint(equalToConstant: Constants.tabBarViewHeight),
+                
+                settingsButton.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -.spacingL),
+                settingsButton.bottomAnchor.constraint(equalTo: bottomAnchor),
+                
+                searchButton.trailingAnchor.constraint(equalTo: settingsButton.leadingAnchor, constant: -.spacingL),
+                searchButton.bottomAnchor.constraint(equalTo: bottomAnchor),
                 
             ])
         }
