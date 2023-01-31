580a1dd6b79b754a966ccef73fedccace512f5d2
TonyJem <62282734+TonyJem@users.noreply.github.com>
2023-01-31 08:26:52 +0200

    Create calendar collection view UI (#1)
    * Create collectionView template
    
    * Update Calendar.View.swift
    
    * Update Calendar.View.swift
    
    * Show weekdays in calendar view
    
    * Refactor UICollectionViews
    
    * Update Calendar.View.swift
    
    * Create CollectionViewCell.ViewState
    
    * Update cell UI with viewState.type
    
    * Create UI logic showing selected date and today's day
    
    * Update dateViewStates
    
    * Update Calendar.CollectionViewCell.ViewState with MonthType

---
+++ myPlanner/Features/Calendar/View/Calendar.Cell.swift
@@ -0,0 +1,119 @@
+import UIKit
+
+extension Calendar {
+    
+    class Cell: UICollectionViewCell {
+        
+        enum Constants {
+            
+            static let circleViewWidth: CGFloat = 25
+            
+        }
+        
+        static let identifier = String(describing: Cell.self)
+        
+        var viewState: CollectionViewCell.ViewState? {
+            didSet {
+                render(viewState: viewState)
+            }
+        }
+        
+        // MARK: - SubViews
+        
+        private lazy var circleView: UIView = {
+            let view = UIView().autolayout()
+            view.layer.cornerRadius = Constants.circleViewWidth / 2;
+            view.layer.masksToBounds = true;
+            return view
+        }()
+        
+        private lazy var label: UILabel = {
+            let label = UILabel().autolayout()
+            label.numberOfLines = 1
+            label.textAlignment = .center
+            return label
+        }()
+        
+        // MARK: - Init
+        override init(frame: CGRect) {
+            super.init(frame: frame)
+            setup()
+            setConstraints()
+        }
+        
+        required init?(coder: NSCoder) {
+            fatalError("init(coder:) has not been implemented")
+        }
+        
+        // MARK: - Private Methods
+        private func setup() {
+            contentView.backgroundColor = .white
+            addSubview(circleView)
+            addSubview(label)
+        }
+        
+        private func render(viewState: CollectionViewCell.ViewState?) {
+            
+            guard let viewState = viewState else { return }
+            
+            if viewState.isToday {
+                circleView.backgroundColor = .systemOrange
+            } else {
+                circleView.backgroundColor = .systemGray
+            }
+            
+            circleView.isHidden = !viewState.isSelected
+            
+            label.text = viewState.text
+            
+            switch viewState.cellType {
+            case .weekDay:
+                label.textColor = .systemGray
+                label.font = UIFont.systemFont(ofSize: 13, weight: .light)
+            case .date:
+                label.font = UIFont.systemFont(ofSize: 15, weight: .light)
+                if viewState.isSelected {
+                    label.textColor = .white
+                } else {
+                    label.textColor = .black
+                }
+                if viewState.isToday {
+                    label.textColor = .systemOrange
+                }
+                
+                if viewState.isToday && viewState.isSelected {
+                    label.textColor = .white
+                }
+            }
+            
+            switch viewState.monthType {
+            case .previousMonth, .nextMonth:
+                label.textColor = .systemGray
+            case .currentMonth:
+                break
+            }
+            
+        }
+        
+    }
+    
+}
+
+// MARK: - SetConstraints
+extension Calendar.Cell {
+    
+    private func setConstraints() {
+        NSLayoutConstraint.activate([
+            
+            circleView.widthAnchor.constraint(equalToConstant: Constants.circleViewWidth),
+            circleView.heightAnchor.constraint(equalTo: circleView.widthAnchor),
+            circleView.centerXAnchor.constraint(equalTo: centerXAnchor),
+            circleView.centerYAnchor.constraint(equalTo: centerYAnchor),
+            
+            label.centerXAnchor.constraint(equalTo: centerXAnchor),
+            label.centerYAnchor.constraint(equalTo: centerYAnchor)
+            
+        ])
+    }
+    
+}
--- myPlanner/Features/Calendar/View/Calendar.View.swift
+++ myPlanner/Features/Calendar/View/Calendar.View.swift
@@ -4,24 +4,88 @@ extension Calendar {
     
     class View: UIView {
         
-        // MARK: - SubViews
-        private lazy var title: UILabel = {
-            let label = UILabel().autolayout()
-            label.text = "Calendar"
-            return label
-        }()
+        enum Constants {
+            
+            static let weekDaysRowHeightMultiplier: CGFloat = 1/7
+            static let numberOfRows: CGFloat = 7
+            static let numberOfCollumns: CGFloat = 7
+            
+        }
         
-        private lazy var collectionView: UICollectionView = {
-            let layout = UICollectionViewFlowLayout()
-            var collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout).autolayout()
-            return collectionView
-        }()
+        private let weekDayViewStates: [CollectionViewCell.ViewState] = [
+            CollectionViewCell.ViewState(text: "Mon", cellType: .weekDay),
+            CollectionViewCell.ViewState(text: "Tue", cellType: .weekDay),
+            CollectionViewCell.ViewState(text: "Wed", cellType: .weekDay),
+            CollectionViewCell.ViewState(text: "Thu", cellType: .weekDay),
+            CollectionViewCell.ViewState(text: "Fri", cellType: .weekDay),
+            CollectionViewCell.ViewState(text: "Sat", cellType: .weekDay),
+            CollectionViewCell.ViewState(text: "Sun", cellType: .weekDay)
+        ]
+        
+        private let dateViewStates: [CollectionViewCell.ViewState] = [
+            CollectionViewCell.ViewState(text: "26", monthType: .previousMonth),
+            CollectionViewCell.ViewState(text: "27", monthType: .previousMonth),
+            CollectionViewCell.ViewState(text: "28", monthType: .previousMonth),
+            CollectionViewCell.ViewState(text: "29", monthType: .previousMonth),
+            CollectionViewCell.ViewState(text: "30", monthType: .previousMonth),
+            CollectionViewCell.ViewState(text: "31", monthType: .previousMonth),
+            CollectionViewCell.ViewState(text: "1"),
+            CollectionViewCell.ViewState(text: "2"),
+            CollectionViewCell.ViewState(text: "3"),
+            CollectionViewCell.ViewState(text: "4"),
+            CollectionViewCell.ViewState(text: "5"),
+            CollectionViewCell.ViewState(text: "6"),
+            CollectionViewCell.ViewState(text: "7"),
+            CollectionViewCell.ViewState(text: "8"),
+            CollectionViewCell.ViewState(text: "9", isSelected: true),
+            CollectionViewCell.ViewState(text: "10"),
+            CollectionViewCell.ViewState(text: "11"),
+            CollectionViewCell.ViewState(text: "12"),
+            CollectionViewCell.ViewState(text: "13"),
+            CollectionViewCell.ViewState(text: "14"),
+            CollectionViewCell.ViewState(text: "15"),
+            CollectionViewCell.ViewState(text: "16"),
+            CollectionViewCell.ViewState(text: "17"),
+            CollectionViewCell.ViewState(text: "18"),
+            CollectionViewCell.ViewState(text: "19"),
+            CollectionViewCell.ViewState(text: "20"),
+            CollectionViewCell.ViewState(text: "21"),
+            CollectionViewCell.ViewState(text: "22", isToday: true),
+            CollectionViewCell.ViewState(text: "23"),
+            CollectionViewCell.ViewState(text: "24"),
+            CollectionViewCell.ViewState(text: "25"),
+            CollectionViewCell.ViewState(text: "26"),
+            CollectionViewCell.ViewState(text: "27"),
+            CollectionViewCell.ViewState(text: "28"),
+            CollectionViewCell.ViewState(text: "29"),
+            CollectionViewCell.ViewState(text: "30"),
+            CollectionViewCell.ViewState(text: "31"),
+            CollectionViewCell.ViewState(text: "1", monthType: .nextMonth),
+            CollectionViewCell.ViewState(text: "2", monthType: .nextMonth),
+            CollectionViewCell.ViewState(text: "3", monthType: .nextMonth),
+            CollectionViewCell.ViewState(text: "4", monthType: .nextMonth),
+            CollectionViewCell.ViewState(text: "5", monthType: .nextMonth)
+        ]
+        
+        // MARK: - SubViews
+        
+        private lazy var weekDays = createCollectionView()
+        private lazy var dates = createCollectionView()
         
         // MARK: - Init
         override init(frame: CGRect) {
             super.init(frame: frame)
             setupViews()
             setConstraints()
+            
+            weekDays.dataSource = self
+            weekDays.delegate = self
+            weekDays.register(Cell.self, forCellWithReuseIdentifier: Cell.identifier)
+            
+            dates.dataSource = self
+            dates.delegate = self
+            dates.register(Cell.self, forCellWithReuseIdentifier: Cell.identifier)
+            
         }
         
         required init?(coder: NSCoder) {
@@ -31,10 +95,22 @@ extension Calendar {
         // MARK: - Private Methods
         private func setupViews() {
             backgroundColor = .white
+            addSubview(weekDays)
+            addSubview(dates)
+        }
+        
+        /// Creates default CollectionView.
+        private func createCollectionView() -> UICollectionView {
+            let layout = UICollectionViewFlowLayout()
+            layout.minimumLineSpacing = .zero
+            layout.minimumInteritemSpacing = .zero
             
-            addSubview(collectionView)
-            
-            addSubview(title)
+            let collectionView = UICollectionView(frame: frame, collectionViewLayout: layout).autolayout()
+            collectionView.backgroundColor = .white
+            collectionView.showsVerticalScrollIndicator = false
+            collectionView.showsHorizontalScrollIndicator = false
+            collectionView.isScrollEnabled = false
+            return collectionView
         }
         
     }
@@ -47,16 +123,63 @@ extension Calendar.View {
     private func setConstraints() {
         NSLayoutConstraint.activate([
             
-            title.centerXAnchor.constraint(equalTo: centerXAnchor),
-            title.topAnchor.constraint(equalTo: topAnchor, constant: .spacingS),
-            title.widthAnchor.constraint(lessThanOrEqualTo: widthAnchor),
+            weekDays.leadingAnchor.constraint(equalTo: leadingAnchor),
+            weekDays.topAnchor.constraint(equalTo: topAnchor),
+            weekDays.trailingAnchor.constraint(equalTo: trailingAnchor),
+            weekDays.heightAnchor.constraint(
+                equalTo: heightAnchor,
+                multiplier: Constants.weekDaysRowHeightMultiplier
+            ),
             
-            collectionView.leadingAnchor.constraint(equalTo: leadingAnchor),
-            collectionView.topAnchor.constraint(equalTo: topAnchor),
-            collectionView.trailingAnchor.constraint(equalTo: trailingAnchor),
-            collectionView.bottomAnchor.constraint(equalTo: bottomAnchor)
+            dates.leadingAnchor.constraint(equalTo: leadingAnchor),
+            dates.topAnchor.constraint(equalTo: weekDays.bottomAnchor, constant: -.spacingXS),
+            dates.trailingAnchor.constraint(equalTo: trailingAnchor),
+            dates.bottomAnchor.constraint(equalTo: bottomAnchor)
             
         ])
+    }
+    
+}
+
+// MARK: - CollectionView DataSource
+extension Calendar.View: UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        if collectionView == self.weekDays {
+            return weekDayViewStates.count
+        } else {
+            return dateViewStates.count
+        }
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: Calendar.Cell.identifier, for: indexPath) as! Calendar.Cell
+        
+        if collectionView == self.weekDays {
+            cell.viewState = weekDayViewStates[indexPath.row]
+        } else {
+            cell.viewState = dateViewStates[indexPath.row]
+        }
+        
+        return cell
+    }
+    
+}
+
+// MARK: - CollectionView Delegate
+extension Calendar.View: UICollectionViewDelegate {
+    
+}
+
+// MARK: - CollectionView DelegateFlowLayout
+extension Calendar.View: UICollectionViewDelegateFlowLayout {
+    
+    // Returns Item size
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(
+            width: frame.width / Constants.numberOfCollumns,
+            height: frame.height / Constants.numberOfRows
+        )
     }
     
 }
