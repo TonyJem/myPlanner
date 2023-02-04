52f688e89ed9317280584df07b18211199f7457e
TonyJem <a.jemanov@gmail.com>
2023-02-04 07:49:03 +0200

    Add comment for better explanation of TabButton

--- myPlanner/DesignSystem/Elements/TabButton.swift
+++ myPlanner/DesignSystem/Elements/TabButton.swift
@@ -1,20 +1,24 @@
 import UIKit
 
-//TODO: Write comments for each step, so it will be easy to understand algorithm later, while it will be needed to look at it again next time
 protocol TabButtonProtocol: UIButton {
     
+    /// Holds the ViewState of the `TabButton`
     var viewState: TabButton.ViewState { get set }
     
 }
 
-class TabButton: UIButton, TabButtonProtocol {
+/// This class defines customised button, that has shape of Trapezium with rounded corners.
+/// Used for Tab's shape visualisation in UI.
+final class TabButton: UIButton, TabButtonProtocol {
     
+    /// Holds internal constants, that will be used same for all instances of `TabButton`.
+    /// Adjustments of it will cause changes in each `TabButton` used in App.
     enum Constants {
         
-        /// Tab rounding radius.
+        /// Describes the radius for rounding left and right top corners of Tab.
         static let tabCornerRadius: CGFloat = 10.0
         
-        /// Descrbes tab side alignment angle measured in degrees:
+        /// Describes Tab side's alignment angle measured in degrees:
         static let tabAlignmentAngle: CGFloat = 15.0
         
     }
@@ -40,7 +44,8 @@ class TabButton: UIButton, TabButtonProtocol {
     
     // MARK: - Actions
     
-    @objc private func didTapOnTabButton() {
+    /// The action that will happen after tapping on particular instance of `TabButton`.
+    @objc private func didTapAction() {
         print("🟢 didTapOnTabButton in TabButton")
     }
     
@@ -48,12 +53,12 @@ class TabButton: UIButton, TabButtonProtocol {
     
     private func setup() {
         backgroundColor = .clear
-        setTitleColor(.white, for: .normal)
-        addTarget(self, action: #selector(didTapOnTabButton), for: .touchUpInside)
+        addTarget(self, action: #selector(didTapAction), for: .touchUpInside)
     }
     
     private func render(viewState: ViewState) {
         setTitle(viewState.title, for: .normal)
+        setTitleColor(viewState.color, for: .normal)
         reloadLayer()
     }
     
@@ -89,19 +94,22 @@ extension TabButton {
         let type: TabButtonType
         let title: String
         let color: UIColor
+        let textColor: UIColor
         let isActive: Bool
         
         static let initial: ViewState = ViewState(
             type: .top,
             title: "Title",
             color: .tabDayBackround,
+            textColor: .white,
             isActive: false
         )
         
-        init(type: TabButtonType, title: String, color: UIColor, isActive: Bool = false) {
+        init(type: TabButtonType, title: String, color: UIColor, textColor: UIColor, isActive: Bool = false) {
             self.type = type
             self.title = title
             self.color = color
+            self.textColor = textColor
             self.isActive = isActive
         }
         
@@ -118,6 +126,7 @@ extension TabButton {
         /// Converts `tabAlignmentAngle` value from degrees into radians.
         let alignmentAngle: CGFloat = Constants.tabAlignmentAngle * .pi / 180
         
+        // Selects `TabButton` shape's drawing method depending on type of `TabButton`:
         switch viewState.type {
         case .top:
             drawTopTabButton(angle: alignmentAngle, size: rect.size)
@@ -125,6 +134,7 @@ extension TabButton {
             drawBottomTabButton(angle: alignmentAngle, size: rect.size)
         }
         
+        // Brings active tab in front of other tabs located near by:
         if viewState.isActive {
             superview?.bringSubviewToFront(self)
         }
@@ -181,6 +191,34 @@ extension TabButton {
         )
     }
     
+    /// Reminder about `iOS Coordinate system`:
+    /// (0,0) is top left corner.
+    /// so Positive X axis - from 0 to the right of screen.
+    /// so Positive Y axis - from 0 to the bottom of screen.
+    ///
+    /// Angles in this coordinate system will be described as following:
+    /// Positive (+) angle direction in iOS is going clockwise.
+    /// Negative (-) angle direction in iOS is going anti-clockwise.
+    ///
+    /// Positive X axis: Angle 0 --> right of the screen;
+    /// Negative Y axis: Angle (normal human 90), but in iOS considered as an Angle: 270 or -90 --> looks into top of the screen;
+    /// Negative X axis: Angle 180  --> looks into left of the screen;
+    /// Postive Y axis: Angle (normal human 270), but in iOS considered as an Angle: 90 or -270  --> bottom of the screen.
+    
+    /// Defines the algorithm of creating path
+    /// used for drawing trapezium shape with rounded corners.
+    /// All points below are described in drawing provided at Project Documentation.
+    /// - Parameters:
+    ///  - startBaseLine: Point `A` coordinates.
+    ///  - endBaseLine: Point `B` coordinates.
+    ///  - leftArcCenter: Point `C1` coordinates.
+    ///  - rightArcCenter: Point `C2` coordinates.
+    ///  - cornerRadius: Radius of trapezium shape corner's rounding.
+    ///  - startAngle1: Angle between positive `X`axis and `C1D1` in radians.
+    ///  - endAngle1: Angle between positive `X`axis and `C1E1` or `C2E2` in radians.
+    ///  - endAngle2: Angle between positive `X`axis and `C2D2` in radians.
+    ///  - clockwise: Direction between `startAngle` and `endAngle` needed to define which sector of the full circle should be selected as an arc.
+    ///  - color: Background color of trapezium shape.
     private func createTrapeziumPath(
         startBaseLine: CGPoint,
         endBaseLine: CGPoint,
