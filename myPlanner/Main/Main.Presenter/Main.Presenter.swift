fd58afa2548098e640970a58e789990c61bef39b
TonyJem <a.jemanov@gmail.com>
2023-02-06 13:26:52 +0200

    Update Presenter with createInitialMainViewState

--- myPlanner/Main/Main.Presenter/Main.Presenter.swift
+++ myPlanner/Main/Main.Presenter/Main.Presenter.swift
@@ -21,7 +21,23 @@ extension Main {
 extension Main.Presenter: MainPresenter {
     
     func viewDidLoad() {
-        view?.render(viewState: .initial)
+        view?.render(viewState: createInitialMainViewState())
+    }
+    
+}
+
+// MARK:
+extension Main.Presenter {
+    
+    private func createInitialMainViewState() -> Main.ViewState {
+        .init(headerViewState: Header.View.ViewState(
+            tabBarViewState: TabBarView.ViewState(
+                type: .top,
+                tabViewStates:[
+                    TabButton.ViewState(type: .top, title: "MainTab1", color: .green),
+                    TabButton.ViewState(type: .top, title: "MainTab2", color: .blue),
+                    TabButton.ViewState(type: .top, title: "MainTab3", color: .magenta)
+                ])))
     }
     
 }
