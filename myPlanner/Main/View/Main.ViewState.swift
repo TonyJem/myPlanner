2ef9957bfc29e23944869fee9a86097cac740098
TonyJem <a.jemanov@gmail.com>
2023-02-06 13:30:17 +0200

    Remove static let initial from Main.ViewState

--- myPlanner/Main/View/Main.ViewState.swift
+++ myPlanner/Main/View/Main.ViewState.swift
@@ -6,19 +6,6 @@ extension Main {
         
         let headerViewState: Header.View.ViewState
         
-        static let initial: ViewState = ViewState(headerViewState: Header.View.ViewState(
-            tabBarViewState: TabBarView.ViewState(
-                type: .top,
-                tabViewStates:[
-                    TabButton.ViewState(type: .top, title: "MainTab1", color: .green),
-                    TabButton.ViewState(type: .top, title: "MainTab2", color: .blue),
-                    TabButton.ViewState(type: .top, title: "MainTab3", color: .magenta)
-                ])))
-        
-        init(headerViewState: Header.View.ViewState) {
-            self.headerViewState = headerViewState
-        }
-        
     }
     
 }
