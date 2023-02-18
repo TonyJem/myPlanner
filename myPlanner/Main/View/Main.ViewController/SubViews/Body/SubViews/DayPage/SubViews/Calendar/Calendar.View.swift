f4cea602a8454e0a2e3c6acca65295cca3786361
TonyJem <a.jemanov@gmail.com>
2023-02-18 10:57:56 +0200

    Create Calendar View templates

---
+++ myPlanner/Main/View/Main.ViewController/SubViews/Body/SubViews/DayPage/SubViews/Calendar/Calendar.View.swift
@@ -0,0 +1,7 @@
+import UIKit
+
+extension DayPage.Calendar {
+    
+    final class View: UIView { }
+    
+}
---
+++ myPlanner/Main/View/Main.ViewController/SubViews/Body/SubViews/DayPage/SubViews/Calendar/Calendar.ViewState.swift
@@ -0,0 +1,7 @@
+import Foundation
+
+extension DayPage.Calendar {
+    
+    struct ViewState { }
+    
+}
