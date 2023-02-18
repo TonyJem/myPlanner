96b5e2a7e1a7501acfa1382624b7f44cdec821a1
TonyJem <a.jemanov@gmail.com>
2023-02-18 10:00:14 +0200

    Create DayPage.NewView

--- myPlanner/Main/View/Main.ViewController/SubViews/Body/SubViews/DayPage/DayPage.View.swift
+++ myPlanner/Main/View/Main.ViewController/SubViews/Body/SubViews/DayPage/DayPage.View.swift
@@ -2,6 +2,82 @@ import UIKit
 
 extension DayPage {
     
+    final class NewView: UICollectionView {
+        
+        enum Constants {
+            
+            static let minimumLineSpacing: CGFloat = 0.0
+            
+        }
+        
+        // MARK: - Init
+        init() {
+            let layout = UICollectionViewFlowLayout()
+            layout.scrollDirection = .horizontal
+            layout.minimumLineSpacing = Constants.minimumLineSpacing
+            super.init(frame: .zero, collectionViewLayout: layout)
+            
+            backgroundColor = .white
+            isPagingEnabled = true
+            showsVerticalScrollIndicator = false
+            showsHorizontalScrollIndicator = false
+            
+            dataSource = self
+            delegate = self
+            
+            register(ViewCell.self, forCellWithReuseIdentifier: ViewCell.identifier)
+        }
+        
+        required init?(coder: NSCoder) {
+            fatalError("init(coder:) has not been implemented")
+        }
+    }
+}
+
+// MARK: - CollectionView DataSource
+extension DayPage.NewView: UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 888
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: Day.ViewCell.identifier, for: indexPath) as! Day.ViewCell
+        
+        let title = "Day: \(indexPath.row)"
+        let date = "\(indexPath.row)"
+        let subtitle = "\(indexPath.row)TH DAY, 337 LEFT, WEEK 4"
+        
+        let viewState = DateModul.ViewState(
+            title: title,
+            date: date,
+            subtitle: subtitle
+        )
+        
+        cell.renderDate(viewState: viewState)
+        
+        return cell
+    }
+}
+
+// MARK: - CollectionView Delegate
+extension DayPage.NewView: UICollectionViewDelegate {
+    
+}
+
+// MARK: - CollectionView DelegateFlowLayout
+extension DayPage.NewView: UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: frame.width, height: frame.height)
+    }
+}
+
+// TODO: - OLD View. Need to remove it! -
+
+extension DayPage {
+    
     class View: UIView {
         
         // MARK: - SubViews
---
+++ myPlanner/Main/View/Main.ViewController/SubViews/Body/SubViews/DayPage/DayPage.ViewCell.swift
@@ -0,0 +1,112 @@
+import UIKit
+
+extension DayPage {
+    
+    final class ViewCell: UICollectionViewCell {
+        
+        enum Constants {
+            
+            static let dateViewWidthMultiplier: CGFloat = 0.25
+            static let dateViewHeightMultiplier: CGFloat = 6/7
+            
+        }
+        
+        static let identifier = "ViewCellIdentifier"
+        
+        // MARK: - SubViews
+        private lazy var dateView: DateModul.View = {
+            let view = DateModul.View().autolayout()
+            return view
+        }()
+        
+        private lazy var calendarView: Calendar.View = {
+            let view = Calendar.View().autolayout()
+            return view
+        }()
+        
+        private lazy var tableView: UIView = {
+            let view = UIView().autolayout()
+            view.backgroundColor = .systemCyan
+            return view
+        }()
+        
+        private lazy var quoteView: UIView = {
+            let view = UIView().autolayout()
+            view.backgroundColor = .systemMint
+            return view
+        }()
+        
+        private lazy var eventsView: UIView = {
+            let view = UIView().autolayout()
+            view.backgroundColor = .systemPurple
+            return view
+        }()
+        
+        // MARK: - Init
+        override init(frame: CGRect) {
+            super.init(frame: frame)
+            
+            setupViews()
+            setConstraints()
+        }
+        
+        required init?(coder: NSCoder) {
+            fatalError("init(coder:) has not been implemented")
+        }
+        
+        // MARK: - Public Methods
+        func renderDate(viewState: DateModul.ViewState) {
+            dateView.renderDate(viewState: viewState)
+        }
+        
+        // MARK: - Private Methods
+        private func setupViews() {
+            addSubview(dateView)
+            addSubview(calendarView)
+            addSubview(tableView)
+            addSubview(quoteView)
+            addSubview(eventsView)
+        }
+        
+    }
+    
+}
+
+// MARK: - SetConstraints
+extension Day.ViewCell {
+    
+    private func setConstraints() {
+        NSLayoutConstraint.activate([
+            
+            dateView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            dateView.topAnchor.constraint(equalTo: topAnchor),
+            dateView.widthAnchor.constraint(equalTo: widthAnchor,
+                                            multiplier: Constants.dateViewWidthMultiplier),
+            dateView.heightAnchor.constraint(equalTo: dateView.widthAnchor,
+                                             multiplier: Constants.dateViewHeightMultiplier),
+
+            calendarView.leadingAnchor.constraint(equalTo: dateView.trailingAnchor),
+            calendarView.topAnchor.constraint(equalTo: topAnchor),
+            calendarView.widthAnchor.constraint(equalTo: dateView.widthAnchor),
+            calendarView.heightAnchor.constraint(equalTo: dateView.heightAnchor),
+
+            tableView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            tableView.topAnchor.constraint(equalTo: dateView.bottomAnchor),
+            tableView.trailingAnchor.constraint(equalTo: calendarView.trailingAnchor),
+            tableView.bottomAnchor.constraint(equalTo: bottomAnchor),
+
+            quoteView.leadingAnchor.constraint(equalTo: calendarView.trailingAnchor),
+            quoteView.topAnchor.constraint(equalTo: topAnchor),
+            quoteView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            quoteView.heightAnchor.constraint(equalTo: dateView.heightAnchor),
+
+            eventsView.leadingAnchor.constraint(equalTo: calendarView.trailingAnchor),
+            eventsView.topAnchor.constraint(equalTo: quoteView.bottomAnchor),
+            eventsView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            eventsView.bottomAnchor.constraint(equalTo: bottomAnchor)
+            
+        ])
+    }
+    
+}
+
