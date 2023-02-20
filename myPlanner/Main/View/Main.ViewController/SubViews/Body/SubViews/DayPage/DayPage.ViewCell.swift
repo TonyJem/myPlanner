7f273927444eb138aab6e6a3cc79d9f505b1ddf3
TonyJem <a.jemanov@gmail.com>
2023-02-21 00:23:02 +0200

    Create DayPage.Calendar.View

--- myPlanner/Main/View/Main.ViewController/SubViews/Body/SubViews/DayPage/DayPage.ViewCell.swift
+++ myPlanner/Main/View/Main.ViewController/SubViews/Body/SubViews/DayPage/DayPage.ViewCell.swift
@@ -28,8 +28,8 @@ extension DayPage {
             return view
         }()
         
-        private lazy var calendarView: CalendarCollectionViewProtocol = {
-            let view = DayPage.Calendar.CollectionView().autolayout()
+        private lazy var calendarView: DayPage.Calendar.View = {
+            let view = DayPage.Calendar.View().autolayout()
             view.backgroundColor = .systemBlue
             return view
         }()
@@ -120,8 +120,8 @@ extension DayPage {
 extension DayPage.ViewCell {
     
     private func render(viewState: DayPage.ViewCell.ViewState?) {
-        guard let viewState = viewState else { return }
-        calendarView.viewState = viewState.calendarState
+//        guard let viewState = viewState else { return }
+//        calendarView.viewState = viewState.calendarState
     }
     
 }
--- myPlanner/Main/View/Main.ViewController/SubViews/Body/SubViews/DayPage/SubViews/DayPage.Calendar/DayPage.Calendar.View.swift
+++ myPlanner/Main/View/Main.ViewController/SubViews/Body/SubViews/DayPage/SubViews/DayPage.Calendar/DayPage.Calendar.View.swift
@@ -1,76 +1,173 @@
 import UIKit
 
-protocol CalendarViewProtocol: UICollectionView {
-    
-    /// Holds the ViewState of the `DayPage.Calendar`
-    var viewState: DayPage.Calendar.ViewState? { get set }
-    
-}
-
 extension DayPage.Calendar {
     
-    final class CollectionView: UICollectionView {
-        
-        enum Constants {
-            
-            static let minimumLineSpacing: CGFloat = 0.0
-            
-        }
+    final class View: UIView {
         
         // MARK: - Properties
         
-        var viewState: ViewState? {
-            didSet {
-                render(viewState: viewState)
-            }
-        }
+        var collectionView: UICollectionView!
         
+        var sections: [MySection] = [
+        
+            MySection(
+                type: "DayNames",
+                items: [
+                MyItem(title: "Mon"),
+                MyItem(title: "Tue"),
+                MyItem(title: "Wed"),
+                MyItem(title: "Thu"),
+                MyItem(title: "Fri"),
+                MyItem(title: "Sat"),
+                MyItem(title: "Sun")
+            ]),
+            
+            MySection(
+                type: "MonthDays",
+                items: [
+                MyItem(title: "1"),
+                MyItem(title: "2"),
+                MyItem(title: "3"),
+                MyItem(title: "4"),
+                MyItem(title: "5"),
+                MyItem(title: "6"),
+                MyItem(title: "7"),
+                MyItem(title: "8"),
+                MyItem(title: "9"),
+                MyItem(title: "10"),
+                MyItem(title: "11"),
+                MyItem(title: "12"),
+                MyItem(title: "13"),
+                MyItem(title: "14"),
+                MyItem(title: "15"),
+                MyItem(title: "16"),
+                MyItem(title: "17"),
+                MyItem(title: "18"),
+                MyItem(title: "19"),
+                MyItem(title: "20"),
+                MyItem(title: "21"),
+                MyItem(title: "22"),
+                MyItem(title: "23"),
+                MyItem(title: "24"),
+                MyItem(title: "25"),
+                MyItem(title: "26"),
+                MyItem(title: "27"),
+                MyItem(title: "28"),
+                MyItem(title: "29"),
+                MyItem(title: "30"),
+                MyItem(title: "31"),
+                MyItem(title: "32"),
+                MyItem(title: "33"),
+                MyItem(title: "34"),
+                MyItem(title: "35"),
+                MyItem(title: "36"),
+                MyItem(title: "37"),
+                MyItem(title: "38"),
+                MyItem(title: "39"),
+                MyItem(title: "40"),
+                MyItem(title: "41"),
+                MyItem(title: "42")
+            ])
+        
+        ]
+        
+        // MARK: - SubViews
         
         // MARK: - Init
         
-        init() {
+        override init(frame: CGRect) {
+            super.init(frame: frame)
             
-            let layout = UICollectionViewFlowLayout()
-            layout.scrollDirection = .horizontal
-            layout.minimumLineSpacing = Constants.minimumLineSpacing
-            
-            super.init(frame: .zero, collectionViewLayout: layout)
-            setupView()
-            configureCollectionView()
+            setupCollectionView()
         }
         
         required init?(coder: NSCoder) {
             fatalError("init(coder:) has not been implemented")
         }
         
-        
         // MARK: - Private Methods
         
         private func setupView() {
-            backgroundColor = .green
-            
-            isPagingEnabled = true
-            showsVerticalScrollIndicator = false
-            showsHorizontalScrollIndicator = false
         }
         
-        private func configureCollectionView() {
-            dataSource = self
-            delegate = self
-            register(ViewCell.self, forCellWithReuseIdentifier: ViewCell.identifier)
+        private func setupCollectionView() {
+            collectionView = UICollectionView(
+                frame: self.bounds,
+                collectionViewLayout: createCompositionalLayout()
+            )
+            collectionView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+            
+            self.addSubview(collectionView)
+            
+            collectionView.register(ViewCell.self, forCellWithReuseIdentifier: ViewCell.identifier)
+            
+            collectionView.dataSource = self
+            collectionView.delegate = self
+            
+        }
+        
+        private func createCompositionalLayout() -> UICollectionViewLayout {
+            let layout = UICollectionViewCompositionalLayout { (sectionIndex, layoutEnvironment) -> NSCollectionLayoutSection? in
+                let section = self.sections[sectionIndex]
+                switch section.type {
+                default:
+                    return self.createSection()
+                }
+            }
+            return layout
+        }
+        
+        
+        // section -> group -> items -> size
+        private func createSection() -> NSCollectionLayoutSection {
+            let itemSize = NSCollectionLayoutSize(
+                widthDimension: .fractionalWidth(1/7),
+                heightDimension: .fractionalWidth(1/7)
+            )
+            
+            let item = NSCollectionLayoutItem(layoutSize: itemSize)
+            item.contentInsets = NSDirectionalEdgeInsets(top: 1, leading: 1, bottom: 1, trailing: 1)
+            
+            let groupSize = NSCollectionLayoutSize(
+                widthDimension: .fractionalWidth(1.0),
+                heightDimension: .estimated(1.0)
+            )
+            
+            let group = NSCollectionLayoutGroup.vertical(layoutSize: groupSize, subitems: [item])
+            
+            let section = NSCollectionLayoutSection(group: group)
+            section.contentInsets = NSDirectionalEdgeInsets(top: 2, leading: 2, bottom: 2, trailing: 2)
+            return section
         }
         
     }
     
 }
 
+// MARK: - UICollectionViewDataSource
 
-// MARK: - CalendarViewProtocol
-
-extension DayPage.Calendar.CollectionView: CalendarViewProtocol {
+extension DayPage.Calendar.View: UICollectionViewDataSource {
     
-    private func render(viewState: DayPage.Calendar.ViewState?) {
-        
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return sections.count
     }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return sections[section].items.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: DayPage.Calendar.ViewCell.identifier, for: indexPath) as! DayPage.Calendar.ViewCell
+        
+        let section = sections[indexPath.section]
+        let item = section.items[indexPath.item]
+        
+        cell.setLabel(text: item.title)
+        return cell
+    }
+    
+}
+
+extension DayPage.Calendar.View: UICollectionViewDelegate {
     
 }
