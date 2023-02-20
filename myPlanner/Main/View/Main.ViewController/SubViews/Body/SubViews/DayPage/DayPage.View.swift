78157907c21a7c200890b9dec30ec69ed5341a57
TonyJem <a.jemanov@gmail.com>
2023-02-20 08:48:20 +0200

    Fix Layout

--- myPlanner/Main/View/Main.ViewController/SubViews/Body/SubViews/DayPage/DayPage.View.swift
+++ myPlanner/Main/View/Main.ViewController/SubViews/Body/SubViews/DayPage/DayPage.View.swift
@@ -29,7 +29,7 @@ extension DayPage {
         
         init() {
             
-            let layout = AutoInvalidatingLayout()
+            let layout = UICollectionViewFlowLayout()
             layout.scrollDirection = .horizontal
             layout.minimumLineSpacing = Constants.minimumLineSpacing
             
