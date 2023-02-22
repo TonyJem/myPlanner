cd1fa70e1105adeab03ceddef84847d4fe5dbd56
TonyJem <a.jemanov@gmail.com>
2023-02-22 22:57:37 +0200

    Create DayPage.Calendar.CollectionViewCell struct

--- myPlanner/Main/View/Main.ViewController/SubViews/Body/SubViews/DayPage/SubViews/Calendar/Calendar.ViewState.swift
+++ myPlanner/Main/View/Main.ViewController/SubViews/Body/SubViews/DayPage/SubViews/Calendar/Calendar.ViewState.swift
@@ -51,3 +51,25 @@ extension DayPage.Calendar {
     }
     
 }
+
+extension DayPage.Calendar {
+    
+    struct CollectionViewCell {
+        
+        enum Configuration {
+            case previuos
+            case current
+            case currentSelected
+            case today
+            case todaySelected
+            case upcoming
+        }
+        
+        struct ViewState: Hashable {
+            let title: String
+            let config: Configuration?
+        }
+        
+    }
+    
+}
